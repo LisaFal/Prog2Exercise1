919adfa038854a7e474b870b8f9c147163abbcbe
eliasFloFH <elias.damisch@stud.fh-campuswien.ac.at>
2023-03-04 10:17:56 +0100

    added List of genres.

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,5 +1,6 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
+import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +8,37 @@ public class Movie {
     private String title;
     private String description;
     // TODO add more properties here
+
+
+    private List<Genre> genres;
+
+    public enum Genre {
+        ACTION,
+        ADVENTURE,
+        ANIMATION,
+        BIOGRAPHY,
+        COMEDY,
+        CRIME,
+        DRAMA,
+        DOCUMENTARY,
+        FAMILY,
+        FANTASY,
+        HISTORY,
+        HORROR,
+        MUSICAL,
+        MYSTERY,
+        ROMANCE,
+        SCIENCE_FICTION,
+        SPORT,
+        THRILLER,
+        WAR,
+        WESTERN
+    }
+
+
+
+
+
 
     public Movie(String title, String description) {
         this.title = title;
@@ -24,6 +56,8 @@ public class Movie {
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
         // TODO add some dummy data here
+
+
 
         return movies;
     }
